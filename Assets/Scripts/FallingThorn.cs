3047f14d8d866f6118df68fa71ccf2f23caac509
chosungwoo <ejyoo12345@naver.com>
2023-10-18 09:23:58 +0900

    Make a Falling Thorn.

--- Assets/Scripts/FallingThorn.cs
+++ Assets/Scripts/FallingThorn.cs
@@ -5,4 +5,39 @@ using UnityEngine;
 
 public class FallingThorn : MonoBehaviour
 {
+    [SerializeField] private float fallingSpeed;
+    [SerializeField] private float scanRange;
+    [SerializeField] private LayerMask whatIsPlayer;
+    [SerializeField] private Thorn thornObj;
+
+    private bool isCollision;
+
+    private void FixedUpdate()
+    {
+        ScanUpdate();
+    }
+
+    private void ScanUpdate()
+    {
+        if (isCollision)
+        {
+            return;
+        }
+        
+        RaycastHit2D ray = Physics2D.Raycast(transform.position, Vector2.down, scanRange, whatIsPlayer);
+
+        if (ray.transform != null)
+        {
+            Debug.Log("충돌됨!!"); 
+            thornObj.Falling(fallingSpeed);
+            isCollision = true;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        
+        Gizmos.DrawRay(transform.position, Vector3.down * scanRange);
+    }
 }
--- Assets/Scripts/Thorn.cs
+++ Assets/Scripts/Thorn.cs
@@ -1,8 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Thorn : MonoBehaviour
 {
+    private float moveSpeed;
+
+    private bool falling = false;
+
+    private void Update()
+    {
+        if (!falling)
+        {
+            return;
+        }
+
+        FallingUpdate();
+    }
     
+    private void FallingUpdate()
+    {
+        Vector3 nextPos = Vector3.down * moveSpeed * Time.deltaTime;
+
+        transform.position += nextPos;
+    }
+
+    public void Falling(float _moveSpeed)
+    {
+        falling = true;
+
+        moveSpeed = _moveSpeed;
+    }
+
+    private void OnBecameInvisible()
+    {
+        if (falling)
+        {
+            Destroy(transform.parent.gameObject);
+        }
+    }
 }
